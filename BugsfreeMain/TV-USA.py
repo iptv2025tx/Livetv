c2cb9240af9ee70d11ef1bc3b681e75bde11a092
2026 <iptv2025tx@gmail.com>
2026-01-26 11:29:39 -0600

    Update TV-USA.py

--- BugsfreeMain/TV-USA.py
+++ BugsfreeMain/TV-USA.py
@@ -261,8 +261,7 @@ def main():
         "http://bedroombullyonline.com:8880/get.php?username=R9YN4ecdXe&password=2AXQHhrJbQ&type=m3u_plus", 
         "http://wickediptv.xyz/get.php?username=rick&password=rick&type=m3u&output=TS", 
         "https://github.com/BuddyChewChew/sports/raw/refs/heads/main/liveeventsfilter.m3u8",  
-        "http://websafety101.net:5050/get.php?username=total&password=Wk9hXW9b57&type=m3u&output=ts",
-        "https://github.com/iptv2025tx/gen-playlist/raw/refs/heads/main/playlist.m3u", 
+        "http://websafety101.net:5050/get.php?username=total&password=Wk9hXW9b57&type=m3u&output=ts", 
     ]
 
     # Set check_links=False for super speed, True for accuracy
