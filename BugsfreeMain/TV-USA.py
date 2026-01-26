5f5b0bb2155bad5f8ea0ee0b4ee5b77da8b1a640
2026 <iptv2025tx@gmail.com>
2026-01-26 11:07:29 -0600

    Update TV-USA.py

--- BugsfreeMain/TV-USA.py
+++ BugsfreeMain/TV-USA.py
@@ -260,14 +260,10 @@ def main():
         "http://nocable.cc:8080/get.php?username=canglobaltv3&password=globe1344&type=m3u_plus", 
         "http://bedroombullyonline.com:8880/get.php?username=R9YN4ecdXe&password=2AXQHhrJbQ&type=m3u_plus", 
         "http://wickediptv.xyz/get.php?username=rick&password=rick&type=m3u&output=TS", 
-        "http://x-api.cc/get.php?username=Loving&password=Youyou&type=m3u&output=ts", 
-        "http://micaplay.net/get.php?username=juanalcivar&password=juan0903&type=m3u&output=ts", 
-        "http://nocable.cc:8080/get.php?username=sales@dellerstechshop.ca&password=sales@4889&type=m3u_plus", 
         "https://github.com/BuddyChewChew/sports/raw/refs/heads/main/liveeventsfilter.m3u8", 
         "http://185.134.22.150:8080/get.php?username=sw-apuniversaliptv-6741&password=poland&type=m3u&output=ts", 
         "http://websafety101.net:5050/get.php?username=total&password=Wk9hXW9b57&type=m3u&output=ts",
-        "https://github.com/iptv2025tx/gen-playlist/raw/refs/heads/main/playlist.m3u",
-        "http://localiptv.site:85/get.php?username=xchangetomastree2020&password=ZeuYTgaB5NnzqE79&type=m3u&output=ts", 
+        "https://github.com/iptv2025tx/gen-playlist/raw/refs/heads/main/playlist.m3u", 
     ]
 
     # Set check_links=False for super speed, True for accuracy
