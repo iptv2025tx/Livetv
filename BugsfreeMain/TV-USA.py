8c102ba594e2ac278ababe4b56e3aa1162de91ab
2026 <iptv2025tx@gmail.com>
2026-01-30 09:41:42 -0600

    Update TV-USA.py

--- BugsfreeMain/TV-USA.py
+++ BugsfreeMain/TV-USA.py
@@ -262,8 +262,7 @@ def main():
         "http://bedroombullyonline.com:8880/get.php?username=R9YN4ecdXe&password=2AXQHhrJbQ&type=m3u_plus", 
         "http://wickediptv.xyz/get.php?username=rick&password=rick&type=m3u&output=TS", 
         "https://github.com/BuddyChewChew/sports/raw/refs/heads/main/liveeventsfilter.m3u8",  
-        "http://websafety101.net:5050/get.php?username=total&password=Wk9hXW9b57&type=m3u&output=ts", 
-    ]
+            ]
 
     # Set check_links=False for super speed, True for accuracy
     collector = M3UCollector(country="USA", check_links=False)
